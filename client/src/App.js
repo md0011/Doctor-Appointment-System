becdabdf9d60b81d121c3ffc4d6f7516b2a65236
md0011 <mayurd0303@gmail.com>
2024-02-11 14:55:16 +0530

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
-    <div className="bg-dark">
-      <h1 className="text-success">Hello</h1>
-    </div>    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
