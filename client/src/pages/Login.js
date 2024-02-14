f3555cafb165ef45713635dbde1490809feafaa4
md0011 <mayurd0303@gmail.com>
2024-02-14 13:34:50 +0530

    Login User With JWT Token

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "../styles/RegisterStyles.css";
-import { Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Form, Input, message } from "antd";
+// import { useDispatch } from "react-redux";
+// import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  // Form Handler
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  //form handler
+  const onfinishHandler = async (values) => {
+    try {
+      // dispatch(showLoading());
+      const res = await axios.post("/api/v1/user/login", values);
+      // window.location.reload();
+      // dispatch(hideLoading());
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      // dispatch(hideLoading());
+      console.log(error);
+      message.error("something went wrong");
+    }
   };
-
   return (
     <>
       <div className="form-container ">
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModels");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //register callback
 const registerController = async (req, res) => {
@@ -26,6 +27,29 @@ const registerController = async (req, res) => {
   }
 };
 
-  const loginController = () => {};
+// login callback
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "user not found", success: false });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "Invalid Email or Password", success: false });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).send({ message: "Login Success", success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
+  }
+};
 
 module.exports = { loginController, registerController };
