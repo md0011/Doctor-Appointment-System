900e808b6a4cec8643853d18b47249e2726490af
md0011 <mayurd0303@gmail.com>
2024-02-15 14:08:45 +0530

    React Redux Toolkit setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'antd/dist/reset.css'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "antd/dist/reset.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 reportWebVitals();
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,18 +3,18 @@ import "../styles/RegisterStyles.css";
 import { Form, Input, message } from "antd";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 
 const Register = () => {
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // form handler
   const onfinishHandler = async (values) => {
     try {
-      // dispatch(showLoading());
+      dispatch(showLoading());
       const res = await axios.post("/api/v1/user/register", values);
-      // dispatch(hideLoading());
+      dispatch(hideLoading());
       if (res.data.success) {
         message.success("Register Successfully!");
         navigate("/login");
@@ -22,7 +22,7 @@ const Register = () => {
         message.error(res.data.message);
       }
     } catch (error) {
-      // dispatch(hideLoading());
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something Went Wrong");
     }
